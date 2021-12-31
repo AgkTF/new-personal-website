177efd26f7573e3c12f47be6f5a28436e9add17e
agkz1st <ahmed.agk22@gmail.com>
2021-12-31 13:19:48 +0200

    - Added hover effect on the projects photos.

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,18 +44,18 @@ export default function Home() {
         <section>
           <h2 className="font-semibold text-lg">Latest Projects</h2>
 
-          <div className="pb-2 mt-3 flex gap-4 scroll-smooth snap-x max-w-full overflow-x-auto">
-            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+          <div className="p-2 mt-3 flex gap-6 scroll-smooth snap-x max-w-full overflow-x-auto">
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center hover:scale-110 overflow-hidden transition">
               <Image
                 alt="project 1"
                 src="/1.png"
                 layout="fill"
-                className="object-cover rounded-md shadow-md w-full h-full"
+                className="object-cover object-center rounded-xl shadow-md w-full h-full"
                 placeholder="blur"
                 blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAL0lEQVQImWNgYZUoLC4tTIxUZmdgqCqu/////+e75zKZDBlMTU1z8wpyE5IUGPgADMQNuu/bx/YAAAAASUVORK5CYII="
               />
             </div>
-            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center hover:scale-110 overflow-hidden transition">
               <Image
                 alt="project 1"
                 src="/2.png"
@@ -65,7 +65,7 @@ export default function Home() {
                 blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAMklEQVQImQEnANj/ALe1voqLmbaxtODU0QCDfY4RABY/Ljbq5OIA4uHkwsXNp6mz/v//rTQXe3xE0LIAAAAASUVORK5CYII="
               />
             </div>
-            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center hover:scale-110 overflow-hidden transition">
               <Image
                 alt="project 1"
                 src="/3.png"
@@ -75,7 +75,7 @@ export default function Home() {
                 blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAMklEQVQImQEnANj/AOTSzdfXy+Tl1v///QDk5OSjm4dsWEl8goQAeHh0eHNwSkZIAAIDA6cVMy5gNfMAAAAASUVORK5CYII="
               />
             </div>
-            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center hover:scale-110 overflow-hidden transition">
               <Image
                 alt="project 1"
                 src="/4.png"
@@ -85,7 +85,7 @@ export default function Home() {
                 blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAMklEQVQImQEnANj/AAoOGKessnqOlkVibgAAAA7///+FlJUTIigAHyMny87RkZOWYGFmIhcPs1pokPUAAAAASUVORK5CYII="
               />
             </div>
-            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center hover:scale-110 overflow-hidden transition">
               <Image
                 alt="project 1"
                 src="/1.png"
