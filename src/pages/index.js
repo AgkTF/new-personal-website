0189beadafb4e49f3decca4aba13fc3a82fe15eb
agkz1st <ahmed.agk22@gmail.com>
2021-12-31 11:40:53 +0200

    - Added the projects section.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import { PageLayout } from 'components/Layout';
 import { ReactLogo, NextLogo, TailwindLogo } from 'components/Icons';
 
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <PageLayout
@@ -41,6 +43,59 @@ export default function Home() {
 
         <section>
           <h2 className="font-semibold text-lg">Latest Projects</h2>
+
+          <div className="mt-3 flex gap-4 scroll-smooth snap-x max-w-full overflow-x-auto">
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+              <Image
+                alt="project 1"
+                src="/1.png"
+                layout="fill"
+                className="object-cover rounded-md shadow-md w-full h-full"
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAL0lEQVQImWNgYZUoLC4tTIxUZmdgqCqu/////+e75zKZDBlMTU1z8wpyE5IUGPgADMQNuu/bx/YAAAAASUVORK5CYII="
+              />
+            </div>
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+              <Image
+                alt="project 1"
+                src="/2.png"
+                layout="fill"
+                className="object-cover rounded-md shadow-md w-full h-full"
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAMklEQVQImQEnANj/ALe1voqLmbaxtODU0QCDfY4RABY/Ljbq5OIA4uHkwsXNp6mz/v//rTQXe3xE0LIAAAAASUVORK5CYII="
+              />
+            </div>
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+              <Image
+                alt="project 1"
+                src="/3.png"
+                layout="fill"
+                className="object-cover rounded-md shadow-md w-full h-full"
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAMklEQVQImQEnANj/AOTSzdfXy+Tl1v///QDk5OSjm4dsWEl8goQAeHh0eHNwSkZIAAIDA6cVMy5gNfMAAAAASUVORK5CYII="
+              />
+            </div>
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+              <Image
+                alt="project 1"
+                src="/4.png"
+                layout="fill"
+                className="object-cover rounded-md shadow-md w-full h-full"
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAMklEQVQImQEnANj/AAoOGKessnqOlkVibgAAAA7///+FlJUTIigAHyMny87RkZOWYGFmIhcPs1pokPUAAAAASUVORK5CYII="
+              />
+            </div>
+            <div className="relative w-64 h-36 flex-shrink-0 snap-center">
+              <Image
+                alt="project 1"
+                src="/1.png"
+                layout="fill"
+                className="object-cover rounded-md shadow-md w-full h-full"
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAL0lEQVQImWNgYZUoLC4tTIxUZmdgqCqu/////+e75zKZDBlMTU1z8wpyE5IUGPgADMQNuu/bx/YAAAAASUVORK5CYII="
+              />
+            </div>
+          </div>
         </section>
       </div>
     </PageLayout>
