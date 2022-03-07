26fae01934473cc5910df80eb0d4a45218a2071f
agkz1st <ahmed.agk22@gmail.com>
2022-03-07 08:15:45 +0200

    - Now the arrows scrolls the card into view but it's kinda clunky. I'm better off using a pre-built component instead just to save time.

--- src/components/UIElements/ProjectCard/ProjectCard.js
+++ src/components/UIElements/ProjectCard/ProjectCard.js
@@ -2,13 +2,14 @@ import Image from 'next/image';
 
 const ProjectCard = ({ id, cover, name, selectedCard, setSelectedCard }) => {
   return (
-    <div
-      className={`snap-center overflow-hidden transition ${
-        selectedCard === id
-          ? 'scale-110 ring-2 ring-paragraph-light dark:ring-tertiary-light rounded-xl'
-          : 'opacity-50'
-      }`}
-    >
+    // <div
+    //   className={`snap-center overflow-hidden transition ${
+    //     selectedCard === id
+    //       ? 'scale-110 ring-2 ring-paragraph-light dark:ring-tertiary-light rounded-xl'
+    //       : 'opacity-50'
+    //   }`}
+    // >
+    <>
       <button
         type="button"
         className="relative w-64 h-36 flex-shrink-0"
@@ -21,7 +22,8 @@ const ProjectCard = ({ id, cover, name, selectedCard, setSelectedCard }) => {
           className={`object-cover cover object-top rounded-xl shadow-md w-full h-full`}
         />
       </button>
-    </div>
+    </>
+    // </div>
   );
 };
 
--- src/components/UIElements/ProjectsCarousel/ProjectsCarousel.js
+++ src/components/UIElements/ProjectsCarousel/ProjectsCarousel.js
@@ -1,23 +1,64 @@
+import { useRef } from 'react';
+
 import { ProjectCard } from 'components/UIElements';
 
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/outline';
 import * as ScrollArea from '@radix-ui/react-scroll-area';
 
 function ProjectsCarousel({ projects, selectedCard, setSelectedCard }) {
+  const cardRef = useRef(selectedCard);
+
+  const arrowHandler = dir => {
+    const currPIndex = projects.findIndex(p => p.id === selectedCard);
+    let prevIndex;
+    if (currPIndex === 0) {
+      prevIndex = projects.length - 1;
+    } else {
+      prevIndex = currPIndex - 1;
+    }
+
+    let nextIndex;
+    if (currPIndex === projects.length - 1) {
+      nextIndex = 0;
+    } else {
+      nextIndex = currPIndex + 1;
+    }
+
+    if (dir === 'left') {
+      setSelectedCard(projects[prevIndex].id);
+    } else if (dir === 'right') {
+      setSelectedCard(projects[nextIndex].id);
+    }
+
+    if (cardRef.current) {
+      cardRef.current.scrollIntoView({ inline: 'start' });
+    }
+  };
+
   return (
     <div className="relative">
       <ScrollArea.Root type="scroll">
         <ScrollArea.Viewport className="scroll-smooth snap-x">
           <div className="px-12 py-2 pb-6 mt-3 flex gap-6">
             {projects.map((project, i) => (
-              <ProjectCard
+              <div
                 key={i}
-                id={project.id}
-                name={project.name}
-                cover={project.cover}
-                selectedCard={selectedCard}
-                setSelectedCard={setSelectedCard}
-              />
+                className={`snap-center overflow-hidden transition ${
+                  selectedCard === project.id
+                    ? 'scale-110 ring-2 ring-paragraph-light dark:ring-tertiary-light rounded-xl'
+                    : 'opacity-50'
+                }`}
+                ref={selectedCard === project.id ? cardRef : null}
+              >
+                <ProjectCard
+                  // key={i}
+                  id={project.id}
+                  name={project.name}
+                  cover={project.cover}
+                  selectedCard={selectedCard}
+                  setSelectedCard={setSelectedCard}
+                />
+              </div>
             ))}
           </div>
         </ScrollArea.Viewport>
@@ -34,6 +75,7 @@ function ProjectsCarousel({ projects, selectedCard, setSelectedCard }) {
         <button
           type="button"
           className="p-2 rounded-full text-paragraph-light dark:text-paragraph-dark bg-cardBg-light dark:bg-cardBg-dark"
+          onClick={() => arrowHandler('left')}
         >
           <ArrowLeftIcon className="h-5 w-5" />
         </button>
@@ -41,6 +83,7 @@ function ProjectsCarousel({ projects, selectedCard, setSelectedCard }) {
         <button
           type="button"
           className="p-2 rounded-full text-paragraph-light dark:text-paragraph-dark bg-cardBg-light dark:bg-cardBg-dark"
+          onClick={() => arrowHandler('right')}
         >
           <ArrowRightIcon className="h-5 w-5" />
         </button>
